c508a3cebf0f76c522e6a73d3e1f66ff743c4716
CarlosIsra <2019452016@teschi.edu.mx>
2023-09-19 10:56:12 -0600

    1.0

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 
 
+class clientes(models.Model):
+    ID_Clientes = models.IntegerField(primary_key=True, db='ID_Clientes')
+    Nombre = models.CharField(max_length=100,db_column='Nombre')
+    Apellido = models.CharField(max_length=100, db_column='Apellido')
+    Correo_Electronico = models.CharField(max_length=100, db_column='Correo Electronico')
+    Telefono = models.IntegerChoices(max_length=10, db_column= 'Telefono')
